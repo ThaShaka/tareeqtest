429dec1297317a44bfd7620e2b9c02f3862c137b
Karlo Pervan <karlopervan4@gmail.com>
2024-02-13 10:07:58 +0100

    aa

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById("viewFeats").addEventListener("click", ()=> {
     document.getElementsByClassName("features")[0].classList.add("slideIn")
     document.getElementsByTagName("header")[0].classList.add("changeColor")
     document.getElementById("joinus").classList.add("changeBtnColor")
+    document.getElementsByClassName("active")[0].classList.add("changeBorder")
     setTimeout(()=> {
         document.getElementsByClassName("initalText")[0].classList.add("textGoBye")
         document.getElementsByClassName("nextText")[0].classList.add("slideIn2")
