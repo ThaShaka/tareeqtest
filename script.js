04c1a1158ae15361b49d9d12fc640652f251c0e7
Karlo Pervan <karlopervan4@gmail.com>
2024-02-15 10:14:41 +0100

    aaaaa

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-document.getElementById("viewFeats").addEventListener("click", ()=> {
-    document.getElementsByClassName("bg")[0].style.visibility = "hidden"
-    document.getElementsByClassName("features")[0].classList.add("slideIn")
-    document.getElementsByTagName("header")[0].classList.add("changeColor")
-    document.getElementById("joinus").classList.add("changeBtnColor")
-    document.getElementsByClassName("active")[0].classList.add("changeBorder")
-    setTimeout(()=> {
-        document.getElementsByClassName("initalText")[0].classList.add("textGoBye")
-        document.getElementsByClassName("nextText")[0].classList.add("slideIn2")
-    }, 5000)
+window.addEventListener("scroll", setScrollVar)
+window.addEventListener("resize", setScrollVar)
+
+
+function setScrollVar() {
+    const htmlElement = document.documentElement;
+    const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight
+    htmlElement.style.setProperty("--scroll", Math.min(percentOfScreenHeightScrolled*100, 100).toString())
+}
+
+window.addEventListener("scroll", ()=> {
+
+    const htmlElement = document.documentElement;
+    const percentOfScreenHeightScrolled = (htmlElement.scrollTop / htmlElement.clientHeight)*100
+    console.log(percentOfScreenHeightScrolled)
+    if(Math.round(percentOfScreenHeightScrolled)> 10) {
+        document.querySelector("header").classList?.add("changeColor")
+        document.querySelector(".bg").style.opacity="0"
+        document.querySelector(".features").classList?.add("slideIn")
+        document.querySelector("#joinus").classList?.remove("changeBtnColorRev")
+        document.querySelector("#joinus").classList?.add("changeBtnColor")
+        document.querySelector(".active").classList?.add("changeBorder")
+
+    } else {
+        document.querySelector("header").classList?.remove("changeColor")
+        document.querySelector("#joinus").classList?.remove("changeBtnColor")
+        document.querySelector("#joinus").classList?.add("changeBtnColorRev")
+        document.querySelector(".active").classList?.remove("changeBorder")
+        document.querySelector(".bg").style.visibility="1"
+    }
 })
+
+const observer = new IntersectionObserver(entries=> {
+    const textDiv = entries[0];
+    const footer = entries[1]
+    if(textDiv.isIntersecting ) {
+        document.querySelector(".initalText").classList?.add("textGoBye")
+        document.querySelector(".initalText").classList?.remove("textGoByeRev")
+        document.querySelector(".nextText").classList?.add("fadeTextIn")
+        document.querySelector(".nextText").classList?.remove("fadeTextInRev")
+    }
+
+    else {
+        document.querySelector(".initalText").classList?.add("textGoByeRev")
+        document.querySelector(".nextText").classList?.add("fadeTextInRev")
+    }
+})
+
+observer.observe(document.querySelector(".observer"))
+observer.observe(document.querySelector("footer"))
+
+
+
+setScrollVar();
